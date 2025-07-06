cfdb3c366eb0abda9fcd7c7bcd943144dc8d0627
patafiole <claude.pastre@free.fr>
2025-07-06 16:00:34 +0200

    adaptation android 15

--- app/src/main/java/fr/gumsparis/gumsbleau/UrlsGblo.java
+++ app/src/main/java/fr/gumsparis/gumsbleau/UrlsGblo.java
@@ -6,9 +6,11 @@ public enum UrlsGblo {
     LISTE("https://www.gumsparis.asso.fr/index.php?option=com_gblo&view=listesorties&format=json"),
     LISTEFUTURES("https://www.gumsparis.asso.fr/index.php?option=com_gblo&view=sortiesfutures&format=json");
 //    SORTIE("http://10.0.2.2:8081/index.php?option=com_gblo&view=prochsortie&format=json"),
-//    LISTE("http://10.0.2.2:8081/index.php?option=com_gblo&view=listesorties&format=json");
+//    LISTE("http://10.0.2.2:8081/index.php?option=com_gblo&view=listesorties&format=json"),
+//    LISTEFUTURES("http://10.0.2.2:8081/index.php?option=com_gblo&view=sortiesfutures&format=json");
 //    SORTIE("https://v2.gumsparis.asso.fr/index.php?option=com_gblo&view=prochsortie&format=json"),
-//    LISTE("https://v2.gumsparis.asso.fr/index.php?option=com_gblo&view=listesorties&format=json");
+//    LISTE("https://v2.gumsparis.asso.fr/index.php?option=com_gblo&view=listesorties&format=json"),
+//    LISTEFUTURES("https://v2.gumsparis.asso.fr/index.php?option=com_gblo&view=sortiesfutures&format=json");
 //    API_LOCAL("http://10.0.2.2:8081/index.php");
 
     final private String url ;
